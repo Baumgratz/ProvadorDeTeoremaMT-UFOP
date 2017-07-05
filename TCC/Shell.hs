f9ba37c8a545e2d6fa0529cb2421de678b38de3b
Guilherme Baumgratz Figueiroa <guilhermebf.ufop@gmail.com>
2017-07-05 15:12:08 -0300

    Tipos e readline
    Ultilizacao do readline para o shell, agora e possivel apagar o que escreveu no shell
    Agora toda contexto e prova tem tipo incluso

--- TCC/Shell.hs
+++ TCC/Shell.hs
@@ -1,33 +1,52 @@
 module Shell where
 
+import System.Console.Readline
+
 data Teorema =
      Lit String
    | Imp String Teorema
-      deriving (Show, Eq)
+      deriving (Eq)
 
-type Contexto = [Teorema]
-type Prova = [Teorema]
+data Type =
+   Kind
+   | App Type Type
+   deriving (Eq)
 
+instance Show Type where
+   show Kind = "*"
+   show (App t t1) = show(t) ++ " -> " ++ show(t1)
+
+instance Show Teorema where
+   show (Lit x) = x
+   show (Imp x y) = x ++ " -> " ++ (show y)
+
+type Contexto = [(Teorema, Type)]
+type Prova = [(Teorema, Type)]
+
+-- libanycurse
+-- ReadLine
+-- colocar tipo
+-- poder colocar nome
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
 
-have' :: String -> String -> Bool
-have' _ [] = True
-have' [] _ = False
-have' (a:as) (b:bs)
-   | a == b = have' as bs
+have' :: Eq a => [a] -> [a] -> Bool
+have' []     _      = True
+have' _      []     = False
+have' (b:bs) (a:as)
+   | a == b = have' bs as
    | otherwise = False
 
-have :: String -> String -> Bool
-have _      []     = True
-have []     _      = False
-have (a:as) y@(b:bs)
-   | a == b = if have' as bs then True else have as y
-   | otherwise = have as y
+have :: Eq a => [a] -> [a] -> Bool
+have []     _       = True
+have _      []      = False
+have y@(b:bs) (a:as)
+   | a == b = if have' bs as then True else have y as
+   | otherwise = have y as
 
-string2Teorema :: String -> Teorema -- ARRUMAR FUNCAO
+string2Teorema :: String -> (Teorema, Type) -- ARRUMAR FUNCAO
 string2Teorema [] = error "Nao existe"
-string2Teorema ('\\':ls) = Imp i l
+string2Teorema ('\\':ls) = (Imp i l, App Kind t)
    where
       ff :: String -> String
       ff [] = []
@@ -35,20 +54,20 @@ string2Teorema ('\\':ls) = Imp i l
          | x == '.' = []
          | otherwise = [x] ++ ff xs
       i = ff ls
-      l = s2t $ removeS i ls
-string2Teorema x = Lit x
+      (l,t) = s2t $ removeS ls (i++".")
+string2Teorema x = (Lit x, Kind)
 
-s2t :: String -> Teorema
+s2t :: String -> (Teorema, Type)
 s2t = string2Teorema
 
-removeS' :: String -> String -> String
+removeS' :: Eq a => [a] -> [a] -> [a]
 removeS' s      []       = s
 removeS' []     _        = []
 removeS' x@(a:as) (b:bs)
    | a == b = removeS' as bs
    | otherwise = x
 
-removeS :: String -> String -> String
+removeS :: Eq a => [a] -> [a] -> [a]
 removeS s      []       = s
 removeS (a:as) x@(b:bs)
    | a == b = removeS' as bs
@@ -59,10 +78,10 @@ removeS (a:as) x@(b:bs)
 
 
 havePre :: String -> Bool
-havePre s = have s "premissa"
+havePre = have "premissa"
 
 haveGoal :: String -> Bool
-haveGoal g = have g "goal"
+haveGoal = have "goal"
 
 removePre :: String -> String
 removePre s = tail $ removeS s "premissa"
@@ -70,19 +89,23 @@ removePre s = tail $ removeS s "premissa"
 removeGoal :: String -> String
 removeGoal s = tail $ removeS s "goal"
 
+readl :: IO String
+readl = do
+   x <- readline ""
+   return $ maybe (error "Não conseguiu ler o que digitou") id x
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
 
 contexto' :: String -> Contexto -> IO (String,Contexto) -- return (String, Contexto)
 contexto' f cont
    | havePre f = do
-      s <- getLine
+      s <- readl
       contexto' s $ cont ++ [s2t $ removePre f]
    | otherwise = return (f,cont)
 
 contexto :: IO (String,Contexto)
 contexto = do
-   s <- getLine
+   s <- readl
    contexto' s []
 
 preContexto :: IO (String,Contexto)
@@ -96,42 +119,80 @@ preContexto = do
 provar :: String -> Contexto -> IO (String, Prova)
 provar f cont
    | haveGoal f = do
-      s <- getLine
+      s <- readl
       provar s $ cont ++ [s2t $ removeGoal f]
    | otherwise = return (f, cont)
-
--- provar :: String -> IO Prova
--- provar s = do
---    s <- getLine
---    provar' s []
 
 preProvar :: String -> IO (String, Prova)
 preProvar s = do
    cont <- provar s []
    return cont
+
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
 
 evalProva :: Contexto -> Prova -> (Contexto, Prova)
 evalProva c []             = (c,[])
-evalProva c ((Imp a b):xs) = evalProva (c ++ [Lit a]) (b:xs)
+evalProva c (((Imp a b), App t t1):xs) = evalProva (x ++ [(Lit a, t)]) ((b, t1):xs)
    where
       (x,y) = evalProva c xs
 evalProva c (x:xs) = (i, x:j)
    where
       (i,j) = evalProva c xs
 
-analiseProvar :: Contexto -> Prova -> IO (Contexto, Prova)
+analiseProvar :: Contexto -> Prova -> IO (Contexto, Prova) -- melhorar essa funcao!!!!
 analiseProvar c p  = return $ evalProva c p
+
+analiseProvar' :: Contexto -> Prova -> IO (Contexto, Prova)
+analiseProvar' c [] = return (c, [])
+analiseProvar' c (((Imp a b), App t t1):xs) = do
+   (x,y) <- analiseProvar' c xs
+   putStrLn $ "Ultilizando a regra da introducao da implicacao o literal " ++ a ++
+      " foi colocado no contexto e agora tem que provar " ++ (show b)
+   analiseProvar (x ++ [(Lit a, t)]) ((b, t1):xs)
+analiseProvar' c (x:xs) = do
+   (i, j) <- analiseProvar' c xs
+   return (i, x:j)
+
+---------------------------------------------------------------------
+---------------------------------------------------------------------
+
+remove :: Eq a => a -> [a] -> [a]
+remove x [] = []
+remove x (a:as)
+   | a == x = remove x as
+   | otherwise = a : remove x as
+
+foiProvado :: Contexto -> Prova -> IO Prova
+foiProvado [] p = return p
+foiProvado (x:xs) y
+   | have [x] y = do
+      putStrLn $ "O termo " ++ (show x) ++ " foi provado!"
+      foiProvado xs $ remove x y
+   | otherwise = foiProvado xs y
+
+---------------------------------------------------------------------
+---------------------------------------------------------------------
+
+fim :: IO ()
+fim = do
+   putStrLn $ "Todas as provas ja foram provadas!!"
+
+loop :: String -> Contexto -> Prova -> IO ()
+loop s c p = do
+   putStrLn $ "Contexto: " ++ (show c)
+   putStrLn $ "O que Provar: " ++ (show p)
 
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
 
 main = do
-   putStrLn "Hello!!!"
+   putStrLn "Bem vindo ao assistente de prova!!!"
    (s,c) <- preContexto
    (x,p) <- preProvar s
-   -- s <- getLine
-   putStrLn (show c)
-   putStrLn (show p)
+   (a,b) <- analiseProvar' c p
+   f <- foiProvado a b
+   case f of
+      [] -> fim
+      otherwise -> loop x a f
    putStrLn "Fim"
