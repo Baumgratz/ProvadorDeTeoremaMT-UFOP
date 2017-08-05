aecf7f9e27a03e0f7c8f08e434066a23409887ab
Guilherme Baumgratz Figueiroa <guilhermebf.ufop@gmail.com>
2017-08-04 21:45:20 -0300

    Add files via upload

---
+++ TCC/EvalTerm.hs
@@ -0,0 +1,21 @@
+module EvalTerm (Env,evalInf, evalCheck) where
+
+import TermType
+
+type Env = [Value]
+
+evalInf :: TermInf -> Env -> Value
+evalInf (Ann e _)  d = evalCheck e d
+evalInf (Star)     d = VStar
+evalInf (Pi t t')  d = VPi (evalCheck t d) (\x -> evalCheck t' (x:d))
+evalInf (Free x)   d = vfree x
+evalInf (Bound i)  d =  d !! i
+evalInf (e :@: e') d = vapp (evalInf e d) (evalCheck e' d)
+
+vapp :: Value -> Value -> Value
+vapp (VLam f)     v = f v
+vapp (VNeutral n) v = VNeutral (NApp n v)
+
+evalCheck :: TermCheck -> Env -> Value
+evalCheck (Inf i) d = evalInf i d
+evalCheck (Lam e) d = VLam (\x -> evalCheck e (x:d))
