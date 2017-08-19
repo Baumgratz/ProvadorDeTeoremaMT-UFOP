5527496c8464ad560a714e23704e0a18054e4bf4
Guilherme Baumgratz Figueiroa <guilhermebf.ufop@gmail.com>
2017-08-19 19:28:12 -0300

    Up

--- Lambda Calculo tipado Simples/Term.hs
+++ Lambda Calculo tipado Simples/Term.hs
@@ -2,19 +2,20 @@ module Term where
 
 type Var = String
 
-type Contexto = [(Var, Tipo)]
-
 data Term = V Var
           | Term :@: Term
           | Term ::: Tipo
-          | Term :&: Term
-          | Term :!: Term
+          | Term :*: Term
+          | Term :+: Term
           | Lam Var Term
           | LamT Var Tipo Term
+          | Fst Term
+          | Snd Term
+          | TEither Term Term
       deriving (Show, Eq)
 
 data Tipo = T Var
           | Tipo :>: Tipo
-          | Tipo :$: Tipo
+          | Tipo :&: Tipo
           | Tipo :|: Tipo
       deriving (Show, Eq)
--- Lambda Calculo tipado Simples/Tipagem.hs
+++ Lambda Calculo tipado Simples/Tipagem.hs
@@ -5,6 +5,8 @@ import Control.Monad.Except
 
 type Result a = Either String a
 
+type Contexto = [(Var, Tipo)]
+
 curryTerm :: Contexto -> Term -> Result Tipo
 curryTerm c (V v) = case lookup v c of
                       Just t -> return t
@@ -15,18 +17,53 @@ curryTerm c (t1 :@: t2) = case curryTerm c t1 of
                             _ -> throwError "TIpo errado"
 curryTerm c (LamT v t te) = do a <- curryTerm ((v,t):c) te
                                return (t :>: a)
-curryTerm c (Lam v te) = undefined
+-- curryTerm c (te ::: t@(a :|: b)) = do ti <- curryTerm c te
+--                                       case ((a == ti) || (b == ti)) of
+--                                          True -> return t
+--                                          False -> throwError "Tipo errado"
 curryTerm c (te ::: ti) = do a <- curryTerm c te
                              case (a == ti) of
                                True -> return ti
                                False -> throwError "Tipo errado"
-curryTerm c (t1 :&: t2) = do a <- curryTerm c t1
+curryTerm c (t1 :*: t2) = do a <- curryTerm c t1
                              b <- curryTerm c t2
-                             return (a :$: b)
-curryTerm c (t1 :!: t2) = do a <- curryTerm c t1
+                             return (a :&: b)
+curryTerm c (t1 :+: t2) = do a <- curryTerm c t1
                              b <- curryTerm c t2
                              return (a :|: b)
+curryTerm c (Fst t) = case curryTerm c t of
+                            Right (p1 :&: p2) -> return p1
+                            _ -> throwError "TIpo errado"
+curryTerm c (Snd t) = case curryTerm c t of
+                            Right (p1 :&: p2) -> return p2
+                            _ -> throwError "TIpo errado"
+
 un :: Bool -> Tipo -> String -> Result Tipo
 un b t s
   | b = return t
   | otherwise = throwError s
+
+termCont :: Contexto -> Term -> (Contexto, Term)
+termCont c (a@(V v) ::: t) = ((v,t):c,a)
+termCont c (a@(V _)) = (c,a)
+termCont c (a ::: t) = (x,y:::t)
+  where
+     (x,y) = termCont c a
+termCont c (t1 :@: t2) = (w,y:@:z)
+  where
+     (x,y) = termCont c t1
+     (w,z) = termCont x t2
+termCont c (t1 :*: t2) = (w,y:*:z)
+  where
+     (x,y) = termCont c t1
+     (w,z) = termCont x t2
+termCont c (t1 :+: t2) = (w,y:+:z)
+  where
+     (x,y) = termCont c t1
+     (w,z) = termCont x t2
+termCont c (LamT v s t) = (x,LamT v s y)
+  where
+     (x,y) = termCont c t
+
+frt :: Term -> Term
+frt ((t1 :*: t2) ::: (ti1 :&: ti2) ) = t1 ::: ti1
